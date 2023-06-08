21c7a0a376921289409c58be68afd2b067cf2aa7
tess-phillips <124443397+tess-phillips@users.noreply.github.com>
2023-06-08 12:54:19 +0100

    map working with location

--- tess-trials/something2map.js
+++ tess-trials/something2map.js
@@ -8,13 +8,19 @@ form.addEventListener("submit", (event) => {
     // clear out any previous results
     output.innerHTML = "";
 
+    // store long: -0.157138, lat: 51.593724
     // get the value of the field with name="map"
-    const x = 3; 
-    const y=3; 
-    const z=3;
+
+    function lon2tile(lon,zoom) { return (Math.floor((lon+180)/360*Math.pow(2,zoom))); }
+    function lat2tile(lat,zoom)  { return (Math.floor((1-Math.log(Math.tan(lat*Math.PI/180) + 1/Math.cos(lat*Math.PI/180))/Math.PI)/2 *Math.pow(2,zoom))); }
+    
+    const z=12;
+    const x = lon2tile(-0.157138,z); 
+    const y= lat2tile(51.593724,z); 
 
     // request that map from OpenCycleMap API
-    fetch(`https://tile.thunderforest.com/cycle/${z}/${x}/${y}.png?apikey=98a643ecd1a94eb2b459dc994b6a2de7`)
+    // can do landscape/cycle/...
+    fetch(`https://tile.thunderforest.com/neighbourhood/${z}/${x}/${y}.png?apikey=98a643ecd1a94eb2b459dc994b6a2de7`)
       .then((response) => {
         if (!response.ok) throw new Error(response.status);
         return response.blob()
@@ -41,6 +47,7 @@ form.addEventListener("submit", (event) => {
           output.textContent = "⚠️ Something went wrong";
         }
       });
+      
   });
 
 //   const region_id = 3600065600
