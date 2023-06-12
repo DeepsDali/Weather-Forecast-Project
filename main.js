2ede745bed63da732abc7ad5f0dfa07e69b79c65
tess-phillips <124443397+tess-phillips@users.noreply.github.com>
2023-06-12 11:19:35 +0100

    using the icon api with start and end date

--- main.js
+++ main.js
@@ -43,14 +43,20 @@ form.addEventListener("submit", async (event) => {
     image = newImage; // Update the reference to the new map
     mapDiv.appendChild(newImage);
     const today = new Date();
-    const year = today.getFullYear();
-    const month = String(today.getMonth() + 1).padStart(2, "0");
-    const day = String(today.getDate()).padStart(2, "0");
-    const date = `${year}-${month}-${day}`;
-    // const enddate = `${year}-${month}-${day+7}`;
-    console.log(date);
+    const nextWeek = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000);
 
-    const apiUrl = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset&current_weather=true&timezone=Europe%2FLondon`
+    function formatDate(thedate){
+      const year = thedate.getFullYear();
+      const month = String(thedate.getMonth() + 1).padStart(2, "0");
+      const day = String(thedate.getDate()).padStart(2, "0");
+      return `${year}-${month}-${day}`;
+    }
+
+    const date = formatDate(today)
+    const enddate = formatDate(nextWeek)
+
+    // const apiUrl = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset&current_weather=true&timezone=Europe%2FLondon`
+    const apiUrl = `https://api.open-meteo.com/v1/dwd-icon?latitude=${lat}&longitude=${lon}&hourly=temperature_2m&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset&current_weather=true&timezone=Europe%2FLondon&start_date=${date}&end_date=${enddate}`
 
     const response = await fetch(apiUrl);
 
